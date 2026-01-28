3c930ff2abccde27b134269f446e444577aa2033
tanishaw-max <tanisha.w@inheritx.com>
2026-01-28 23:29:47 +0530

    my Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://task-manager-seven-lime.vercel.app",
     credentials: true,
   })
 );
